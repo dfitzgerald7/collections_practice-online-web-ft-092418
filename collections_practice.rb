49622b5d393307d1b1ab40c0f230abc9c8e1cff0
Doug Fitzgerald <dfitzge1@conncoll.edu>
2018-09-24 15:48:06 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,27 @@
 
+def sort_array_asc(arr)
+  arr.sort 
+end 
+
+def sort_array_desc(arr)
+  arr.sort {|x, y| y <=> x }
+end 
+
+def sort_array_char_count(arr)
+  arr.sort {|x, y| x.length <=> y.length}
+end 
+
+def swap_elements(arr)
+  swap_elements_from_to(arr, 1, 2)
+end 
+
+def swap_elements_from_to(arr, start_index, dest_index)
+  place_holder =  arr[start_index] 
+  arr[start_index] = arr[dest_index]
+  arr[dest_index] = place_holder
+  arr
+end 
+  
+def reverse_array()
+  
+end 
